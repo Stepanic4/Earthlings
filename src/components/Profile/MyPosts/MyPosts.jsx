c49db0ab07c857213c52c86223eed7c6567231be
Zolotukhin Ivan <Zolotukhin.ivan.1@gmail.com>
2023-03-07 00:19:37 +0100

    change button add post

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,16 +10,19 @@ const MyPosts = () => {
                 <img src={rustContent} className={stl.rust} alt="rust"/>
             </div>
             <div className={stl.item}>
-                <img className={stl.postsAva} src="https://www.meme-arsenal.com/memes/a323140cb815b9f6334d34303bb35d93.jpg" alt="avatar"/>
+                <img className={stl.postsAva}
+                     src="https://www.meme-arsenal.com/memes/a323140cb815b9f6334d34303bb35d93.jpg" alt="avatar"/>
                 + Description
             </div>
             <div>
                 <h4>
                     My posts )))
                 </h4>
-                <textarea name="post" id="areaPost" cols="30" rows="4" placeholder='Please enter your post...'>
-                </textarea>
-                <button>Add post</button>
+                <div className={stl.textAreaPost}>
+                    <textarea name="post" id="areaPost" cols="30" rows="4" placeholder='Please enter your post...'>
+                    </textarea>
+                    <button className={stl.myButton}>Add post</button>
+                </div>
             </div>
             <MyOnePost/>
         </div>
