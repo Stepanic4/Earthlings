a610be48850fb7cb95a41561aa042d3afecbe7b7
Zolotukhin Ivan <Zolotukhin.ivan.1@gmail.com>
2023-04-07 23:49:11 +0200

    Transferred data to index.js

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,26 +5,6 @@ import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
-    let dialogs = [
-        {id: 1, name: 'Ivan'},
-        {id: 2, name: 'Lena'},
-        {id: 3, name: 'Arnold'},
-        {id: 4, name: 'Michal'},
-        {id: 5, name: 'Sara'},
-        {id: 6, name: 'Ura'},
-        {id: 7, name: 'Dasha'}
-    ];
-
-    let messages = [
-        {id: 1, message: 'Hello'},
-        {id: 2, message: 'How are you'},
-        {id: 3, message: 'Everything is ok'},
-        {id: 4, message: 'My name Petr'},
-        {id: 5, message: 'If you want?'},
-        {id: 6, message: 'Little car'},
-        {id: 7, message: 'Friends is happy'}
-    ];
-
     let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
     let messagesElements = messages.map(m => <Message message={m.message} id={m.id}/>);
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,26 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+let dialogs = [
+    {id: 1, name: 'Ivan'},
+    {id: 2, name: 'Lena'},
+    {id: 3, name: 'Arnold'},
+    {id: 4, name: 'Michal'},
+    {id: 5, name: 'Sara'},
+    {id: 6, name: 'Ura'},
+    {id: 7, name: 'Dasha'}
+];
+
+let messages = [
+    {id: 1, message: 'Hello'},
+    {id: 2, message: 'How are you'},
+    {id: 3, message: 'Everything is ok'},
+    {id: 4, message: 'My name Petr'},
+    {id: 5, message: 'If you want?'},
+    {id: 6, message: 'Little car'},
+    {id: 7, message: 'Friends is happy'}
+];
+
 let posts = [
     {id: 1, message: 'Hello my friend', likesCount: 2},
     {id: 2, message: 'What are you doing ?', likesCount: 5},
