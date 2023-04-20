75c8f544245a22c9e9a5d16e376ca439b545ca79
Zolotukhin Ivan <Zolotukhin.ivan.1@gmail.com>
2023-04-20 14:57:08 +0200

    myPost.jsx added onClick

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,12 +6,19 @@ const MyPosts = (props) => {
 
     let postsElements = props.posts.map(p => <MyOnePost message={p.message} likesCount={p.likesCount}/>);
 
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert (text)
+    };
+
+    let newPostElement = React.createRef();
+
     return (
         <div className={stl.posts}>
             <div className={stl.textAreaPost}>
-                    <textarea name="post" id="areaPost" cols="30" rows="4" placeholder='Please enter your post...'>
+                    <textarea ref={newPostElement} name="post" cols="30" rows="4" placeholder='Please enter your post...'>
                     </textarea>
-                <button className={stl.myButton}>Add post</button>
+                <button className={stl.myButton} onClick={ addPost }>Add post</button>
             </div>
             <div>
                 <h4>My posts )))</h4>
