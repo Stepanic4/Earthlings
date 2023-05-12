4a7459a8b6b594cd18d59c8265d4dc1b878f06a8
Zolotukhin Ivan <Zolotukhin.ivan.1@gmail.com>
2023-05-13 01:51:20 +0200

    change only avatar on MyOnepost

--- src/components/Profile/MyPosts/MyOnePost/MyOnePost.jsx
+++ src/components/Profile/MyPosts/MyOnePost/MyOnePost.jsx
@@ -3,10 +3,11 @@ import stl from './MyOnePost.module.css';
 
 const MyOnePost = (props) => {
     return (
-        <div className={stl.item}>
-            <img
-                src="https://a.d-cd.net/-Nw4NUI84DSlQW5m8d4vKH7MbxU-1920.jpg"
-                alt="AvaPost"/>
+        <div>
+            <div className={stl.avatar}>
+                <img src="https://a.d-cd.net/-Nw4NUI84DSlQW5m8d4vKH7MbxU-1920.jpg"
+                     alt="AvaPost"/>
+            </div>
             <div className={stl.likePost}>
                 {props.message}
                 <div className={stl.like}>
